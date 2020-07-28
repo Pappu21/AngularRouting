2f0610af7e5b3bf9d56e524188b2f81ac9269046
Pappu21 <pappuchoudhary.kr@gmail.com>
2020-07-28 16:27:03 +0530

    wild route and canactivate

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
@@ -11,17 +11,24 @@ export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
   paramsSubscription: Subscription;
 
-  constructor( private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.user = {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
-    }
+    };
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
   ngOnDestroy() {
-    // this.paramsSubscription.unsubscribe();
+    this.paramsSubscription.unsubscribe();
   }
 
 }
